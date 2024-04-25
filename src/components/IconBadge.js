5d633ed57d7eafa13bd800ee3b582e4522713c70
poojasah77 <poojasah77@gmail.com>
2024-04-25 14:47:32 +0545

    iujyhtgfd

--- src/components/IconBadge.js
+++ src/components/IconBadge.js
@@ -1,7 +1,7 @@
 const IconBadge = ({name}) => {
   return (
     <div className="flex flex-col justify-center place-items-center gap-2 bg-slate-200 px-4 py-3 rounded-md">
-      <img src={name.toLowerCase() + '.png'} alt={name} className="w-12"/>
+      <img src={name + '.png'} alt={name} className="w-12"/>
       <span className="text-black leading-none">{name}</span>
     </div>
   )
